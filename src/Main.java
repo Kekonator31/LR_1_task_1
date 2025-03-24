a470f85ba970c4b99d34fccae05ec274dca336c0
Kekonator31 <48918994+Kekonator31@users.noreply.github.com>
2025-03-24 22:33:35 +0200

    Normal version

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+
+
+class BankAccount {
+    private double balance = 200.55;
+
+    void deposit(double amount){
+        balance = balance + amount;
+        System.out.println("Deposited " + amount + " to the bank.");
+    }
+
+    void getBalance(){
+        System.out.println("Your balance is " + balance);
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount();
+        account.deposit(100);
+        account.balance += 100;
+        account.getBalance();
+    }
+}
